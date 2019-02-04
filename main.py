1a6c4b76650976d74d9585991190bdd0d240dae6
Deirdre Lewis <lewis.deirdre.k@gmail.com>
2019-02-04 10:30:47 -0600

    Added p class errors

--- main.py
+++ main.py
@@ -5,35 +5,28 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-page_header = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Signup</title>
-    </head>
-    <body>    
-        <h1>Signup</h1>
-
-"""
-page_footer = """
-    </body>
-</html>
-"""
-
-add_form = """
+signup_form = """
+    <style>
+        .error {{ color: red; }}
+    </style>
+    <h1>Signup</h1>
     <form action="/add" method = "post">
   
         <label for="username">Username:</label>
         <input id="username" type="text" name="username"/>
+        <p class="error">{username_error}</p>
         </br>
         <label for="password" >Password:</label>
         <input id="password" type="password" name="password"/>
+        <p class="error">{password_error}</p>
         </br>
         <label for="verify">Verify password:</label>
         <input id="verify" type="password" name="verfiy"/>
+        <p class="error">{verify_error}</p>
         </br>
         <label for="email">Email (optional):</label>
         <input id="email" type="text" name="email"/>
+        <p class="error">{email_error}</p>
         </br>
         <button type="submit">Submit</button>
     </form>
